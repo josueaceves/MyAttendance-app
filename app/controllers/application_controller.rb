8bf2f94f294b49214b95f1915fd10d6c037af376
josue aceves <josueaceves.ja@gmail.com>
2016-01-07 21:59:43 -0800

    refactor attendance show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :find_choir_member
 
   protect_from_forgery with: :exception
   def current_user
@@ -10,5 +10,9 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     session[:user_id] != nil
+  end
+
+  def find_choir_member(id)
+    ChoirMember.find_by(id: id)
   end
 end
